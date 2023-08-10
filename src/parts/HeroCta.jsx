3750d75d0f7c5a872c5a085589324070f9c13abb
adolfjeremy <adolf.jer@gmail.com>
2023-08-10 21:58:16 +0700

    create HeroCta component

---
+++ src/parts/HeroCta.jsx
@@ -0,0 +1,18 @@
+import SectionContainer from "../components/SectionContainer";
+
+function HeroCta() {
+    return (
+        <SectionContainer>
+            <div className="cta">
+                <p className="greetings">Hi there, my name is</p>
+                <h1>Jeremy Nainggolan</h1>
+                <p className="title">
+                    Front End Dev & Dedicated Coffee Aficionado,
+                </p>
+                <p className="subtitle">Fueling Code with Caffeine Power.</p>
+            </div>
+        </SectionContainer>
+    );
+}
+
+export default HeroCta;
